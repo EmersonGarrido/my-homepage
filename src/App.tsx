296757b9ed852e12ea97fa95fb50b7a25329d53a
EmersonGarrido <emersongarrido@hotmail.com.br>
2022-08-21 09:39:00 -0400

    :lipstick: feat: Pequena melhoria espacos

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const contactList = [
 const App: React.FC = () => {
   return (
     <div className="p-4 flex items-center justify-center flex-col">
-      <div className="bg-gradient-to-tr from-yellow-400 to-purple-600 p-1 rounded-full mt-5">
+      <div className="bg-gradient-to-tr from-yellow-400 to-purple-600 p-1 rounded-full">
         <img
           src="/assets/avatar.jpeg"
           className="w-[150px] h-[150px] rounded-full border-[#F1F5F7] border-[0.2rem]"
@@ -50,29 +50,41 @@ const App: React.FC = () => {
       </div>
       <div className="text-center mt-2">
         <h1 className="font-bold text-[22px]">Emerson Garrido</h1>
-        <span  className="font-thin text-[16px]">@emersongarrido.dev</span>
+        <span className="font-thin text-[16px]">@emersongarrido.dev</span>
       </div>
       <div className="w-full flex flex-col items-center justify-center mt-5 gap-5 max-w-[400px]">
         {contactList.map((contact, index) => {
           return (
-            <div onClick={() => {
-              if(!contact.disabled) {
-                window.open(`${contact.url}`)
-              }
-            }} className="border-[0.1rem] flex gap-3 items-center rounded-md p-3 w-full cursor-pointer bg-black/5 hover:bg-black/10 shadow-sm">
-              <img src={`/assets/${contact.icon}.png`} className="w-[30px] h-[30px]" />
-            <div className="flex items-center justify-between w-full">
-            <div>{contact.platform}</div>
-            {contact.disabled && <div className="text-black font-thin text-[12px] ">Em breve</div>}
+            <div
+              onClick={() => {
+                if (!contact.disabled) {
+                  window.open(`${contact.url}`);
+                }
+              }}
+              className="border-[0.1rem] flex gap-3 items-center rounded-md p-3 w-full cursor-pointer bg-black/5 hover:bg-black/10 shadow-sm"
+            >
+              <img
+                src={`/assets/${contact.icon}.png`}
+                className="w-[30px] h-[30px]"
+              />
+              <div className="flex items-center justify-between w-full">
+                <div>{contact.platform}</div>
+                {contact.disabled && (
+                  <div className="text-black font-thin text-[12px] ">
+                    Em breve
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-          )
-        })} 
+          );
+        })}
       </div>
       <div className="text-center mt-5 text-[12px]">
         Todos os direitos reservados a vocês que estão lendo isso. <br />
       </div>
-        <span className="font-thin text-[12px]">Este projeto esta open-source no meu GitHub.</span>
+      <span className="font-thin text-[12px]">
+        Este projeto esta open-source no meu GitHub.
+      </span>
     </div>
   );
 };
